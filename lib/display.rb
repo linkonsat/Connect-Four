7da6d61c2d5d3e249565c952233b217284ec5e53
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2021-11-17 14:43:19 -0800

    added UTF-16 codes for chips

---
+++ lib/display.rb
@@ -0,0 +1,9 @@
+
+#this is the black connect for dot
+black_chip = "\uFEFF\u26AB"
+#this is the empty square
+"\uFEFF\u26AA"
+
+#this is the blude dot
+blue_chip = "\uFEFF\u{1F535}"
+
---
+++ lib/symbols.rb
@@ -0,0 +1,15 @@
+module Pieces
+    def black_chip
+        black_chip = "\uFEFF\u26AB"
+        return black_chip
+    end
+
+    def blue_chip
+        blue_chip = "\uFEFF\u{1F535}"
+        return blue_chip
+    end
+
+    def empty_slot
+        empty_slot = "\uFEFF\u26AA"
+        return empty_slot
+    end
