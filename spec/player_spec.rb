bcb862aeea0f51d8f1280b741b4da9e6d6a00514
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2021-11-19 10:38:20 -0800

    Wrote player object tests.

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,5 +1,45 @@
-require_relative "../lib/game"
-
-describe Game do 
-
+require_relative "../lib/player"
+#so this test covers 3 things 1.picking a name 2. picking a symbol 3. selecting a position
+describe Players do 
+    describe "#new_player_name" do
+    subject(:player) { described_class.new }
+    context "Assigns a to the player object" do 
+        it "Assigns a new player name on valid input" do
+        allow(player).to receive(:gets).and_return("Snazzy")
+        player.new_player_name
+        expect(player.name).to eql("Snazzy")
+    end
+    end
+end
+    describe "#assign_chip" do 
+    subject(:player) { described_class.new }
+    context "Assigns the appropriate player input" do 
+    it "Selects the appropriate symbol on valid input" do 
+        allow(player).to receive(:gets).and_return("1")
+        player.assign_chip
+        expect(player.chip).to eql("\uFEFF\u{1F535}")
+    end
+    it "Doesn't assign the appropriate symbol until valid input occurs" do 
+        allow(player).to receive(:gets).and_return("9", "1")
+        player.assign_chip
+        expect(player.chip).to eql("\uFEFF\u{1F535}")
+    end
+end
+end
+    describe "#new_position" do 
+    subject(:player) { described_class.new }
+    context "Assigns the correct position" do 
+        it "returns a valid position" do 
+            allow(player).to receive(:gets).and_return("5")
+            player.new_position
+            expect(player.position).to eql(5)
+        end
+        it "Doesn't assign the position until the valid position is entered" do 
+        allow(player).to receive(:gets).and_return("9", "5")
+        player.new_position
+        player.new_position
+        expect(player.position).to eql(5)
+        end
+    end
+end
 end
