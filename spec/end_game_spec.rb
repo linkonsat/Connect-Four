30394e4250b07c36009678a441257620a97e6677
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2021-11-21 21:48:40 -0800

    Fixed doubles with accurate callable values

--- spec/end_game_spec.rb
+++ spec/end_game_spec.rb
@@ -1,13 +1,12 @@
 require_relative "../lib/end_game"
+require "pry-byebug"
 describe EndGame do
     describe "#vertical_win" do 
     subject(:end_game) { described_class.new }
     it "Returns true on a vertical win" do 
-        board = double("Board")
-        player = double("Player")
-        player.instance_variable_set(:@chip, "a")
-        board.instance_variable_set(:@board, [["[]"],["[]"],["a"],["a"],["a"],["a"]])
-        result = end_game.vertical_win(board,player)
+        board = double("Board", :board => [["[]"],["[]"],["a"],["a"],["a"],["a"]])
+        player = double("Player", :chip => "a")
+        result = end_game.vertical_win(board.board,player.chip)
 
         expect(result).to eql(true)
     end
@@ -15,21 +14,17 @@ end
     describe "#horizontal_win" do 
     subject(:end_game) { described_class.new }
     it "Returns true on a horizontal win" do 
-        board = double("Board")
-        player = double("Player")
-        player.instance_variable_set(:@chip, "a")
-        board.instance_variable_set(:@board, [["[]","[]","a","a","a","a"]])
-        result = end_game.vertical_win(board,player)
+        board = double("Board", :board => [["[]","[]","a","a","a","a"]])
+        player = double("Player", :chip => "a")
+        result = end_game.vertical_win(board.board,player.chip)
         expect(result).to eql(true)
     end
     describe "#diagonal_win" do 
     subject(:end_game) { described_class.new }
     it "Returns true on a diagonal win" do 
-        board = double("Board")
-        player = double("Player")
-        player.instance_variable_set(:@chip, "a")
-        board.instance_variable_set(:@board, [["a"],["[]","a"],["[]","[]","a"],["[]","[]","[]","a"]])
-        result = end_game.vertical_win(board,player)
+        board = double("Board", :board => [["a"],["[]","a"],["[]","[]","a"],["[]","[]","[]","a"]])
+        player = double("Player", :chip => "a")
+        result = end_game.vertical_win(board.board,player.chip)
         expect(result).to eql(true)
     end
 end
@@ -37,11 +32,9 @@ end
     subject(:end_game) { described_class.new }
 end
     it "returns true on no winners" do 
-        board = double("Board")
-        player = double("Player")
-        player.instance_variable_set(:@chip, "a")
-        board.instance_variable_set(:@board,  Array.new(6) {Array.new(7,"[]")})
-        result = end_game.vertical_win(board,player)
+        board = double("Board", :board => Array.new(6) {Array.new(7,"[]")})
+        player = double("Player", :chip => "a")
+        result = end_game.vertical_win(board.board,player.chip)
         expect(result).to eql(true)
     end
 end
