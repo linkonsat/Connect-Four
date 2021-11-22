f154644341450bf37cfd519dd492bccb931152cc
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2021-11-21 22:50:56 -0800

    Wrote methods to detect winning conditions or a tie

--- lib/end_game.rb
+++ lib/end_game.rb
@@ -1,4 +1,60 @@
 #checks for winning conditions or a tie
+require "pry-byebug"
 class EndGame
 
+    def vertical_win?(board,player_chip)
+        board.each_with_index do  |current_row_item, current_row|
+            current_row_item.each_with_index do  |current_column_item,current_column|
+                current_values = [board[current_row][current_column],board[current_row + 1][current_column],
+                                board[current_row + 2][current_column],board[current_row + 3][current_column]]
+                                if(current_values.all?(player_chip))
+                                    return true
+                                end
+                            end
+                            if(board[current_row + 3 ].nil?)
+                                return false
+                            end
+                        end
+                
+    end
+
+    def horizontal_win?(board,player_chip)
+        board.each_with_index do |current_row_item, current_row |
+            current_row_item.each_with_index do |current_column_item, current_column|
+                current_values = [board[current_row][current_column],board[current_row][current_column],
+                                  board[current_row][current_column],board[current_row][current_column]]
+                                  if(current_values.all?(player_chip))
+                                    return true
+                                  elsif(board[current_row][current_column + 3].nil?)
+                                    break
+                                  end
+                                end
+        end
+    end
+
+    def diagonal_win?(board,player_chip)
+        board.each_with_index do |current_row_item, current_row |
+            current_row_item.each_with_index do |current_column_item, current_column|
+                current_values = [board[current_row][current_column],board[current_row + 1][current_column + 1],
+                board[current_row + 2][current_column + 2],board[current_row + 3][current_column + 3]]
+                if (current_values.all?(player_chip))
+                    return true
+                elsif(board[current_row + 3][current_column + 3].nil?)
+                    break
+                end
+            end
+            if (board[current_row + 3].nil?)
+                return false
+            end
+        end
+    end
+
+    def tie?(board)
+        flattened_board = board.flatten
+        if(flattened_board.any?("[]"))
+            return false
+        elsif(flattened_board.none?("[]"))
+            return true
+        end
+    end
 end
--- spec/end_game_spec.rb
+++ spec/end_game_spec.rb
@@ -6,17 +6,16 @@ describe EndGame do
     it "Returns true on a vertical win" do 
         board = double("Board", :board => [["[]"],["[]"],["a"],["a"],["a"],["a"]])
         player = double("Player", :chip => "a")
-        result = end_game.vertical_win(board.board,player.chip)
-
+        result = end_game.vertical_win?(board.board,player.chip)
         expect(result).to eql(true)
     end
 end
     describe "#horizontal_win" do 
     subject(:end_game) { described_class.new }
     it "Returns true on a horizontal win" do 
-        board = double("Board", :board => [["[]","[]","a","a","a","a"]])
+        board = double("Board", :board => [["[]","[]","[]","[]","[]","[]"],["[]","[]","a","a","a","a"]])
         player = double("Player", :chip => "a")
-        result = end_game.vertical_win(board.board,player.chip)
+        result = end_game.horizontal_win?(board.board,player.chip)
         expect(result).to eql(true)
     end
     describe "#diagonal_win" do 
@@ -24,7 +23,7 @@ end
     it "Returns true on a diagonal win" do 
         board = double("Board", :board => [["a"],["[]","a"],["[]","[]","a"],["[]","[]","[]","a"]])
         player = double("Player", :chip => "a")
-        result = end_game.vertical_win(board.board,player.chip)
+        result = end_game.diagonal_win?(board.board,player.chip)
         expect(result).to eql(true)
     end
 end
@@ -32,9 +31,8 @@ end
     subject(:end_game) { described_class.new }
 end
     it "returns true on no winners" do 
-        board = double("Board", :board => Array.new(6) {Array.new(7,"[]")})
-        player = double("Player", :chip => "a")
-        result = end_game.vertical_win(board.board,player.chip)
+        board = double("Board", :board => Array.new(6) {Array.new(7,"a")})
+        result = end_game.tie?(board.board)
         expect(result).to eql(true)
     end
 end
